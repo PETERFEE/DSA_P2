b17f5231ea759d1c0588075e54353e34807f97c0
zenflies <rbisht2006@gmail.com>
2025-11-08 20:35:37 -0500

    added tests

--- predict.py
+++ predict.py
@@ -2,19 +2,19 @@ import joblib
 import numpy as np
 import pandas as pd
 
-model = joblib.load("model/naive_bayes.pkl")
-vectorizer = joblib.load("model/vectorizer.pkl")
-
-while True:
-    message = input("Enter a test message: ")
+def conf(body):
+    model = joblib.load("model/naive_bayes.pkl")
+    vectorizer = joblib.load("model/vectorizer.pkl")
+    #message = input("Enter a test message: ")
+    message = body
     X_new = vectorizer.transform([message])
     preds = model.predict(X_new)
     probs = model.predict_proba(X_new)
     label = preds[0]
     confidence = np.max(probs[0])
-    print(f"Message: {message}")
+    #print(f"Message: {message}")
     #print(f"Predicted label: {label} (confidence: {confidence:.3f})")
     if label == "spam":
-        print(f"Spam probability: {confidence * 100:.2f}%")
+        return confidence
     else:
-        print(f"Spam probability: {100 - confidence * 100:.2f}%")
+        return 1 - confidence
